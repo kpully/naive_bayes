2d02747e86fb8e22b54c8954799c4e0d9c35b833
Katherine Pully <kpully@gmail.com>
2020-04-21 22:20:28 -0500

    updates to test

--- misc.py
+++ misc.py
@@ -3,3 +3,8 @@
 	grouped_counts[3] = data[["a3","a6"]].groupby(by="a3").count().to_dict()["a6"]
 	grouped_counts[4] = data[["a4","a6"]].groupby(by="a4").count().to_dict()["a6"]
 		grouped_counts[1] = data[["a1","a6"]].groupby(by="a1").count().to_dict()["a6"]
+					print("-1*math.log2((" + str(v)+"+0.1)/val_counts[" +str(c)+"]+0.4)")
+					# print("lp["+str(c)+"]["+str(a)+"][val]=" + str(lp[c][a][val]))
+
+				print(numerator)
+				print(denominator)
--- prog4.py
+++ prog4.py
@@ -4,39 +4,74 @@ import math
 import numpy as np
 
 # constants
-DEBUG=False
+DEBUG=True
 
 def main(filename, r_train, r_test):
 	df = pd.read_excel(filename)
 	a6, lp=train(df[:r_train])
+	test(df[r_train:r_test],lp)
 	verbose_output(a6)
 
 
 
 def train(data):
+	if DEBUG:
+		print(data)
 	# get aggregated data
 	val_counts = dict(data["a6"].value_counts())
+	if DEBUG:
+		print(val_counts)
 	# initialize data structs
 	a1,a2,a3,a4,a5,a6 = [-1]*5,[-1]*5,[-1]*5,[-1]*5,[-1]*5,[-1]*5
 	lp=np.zeros((4,6,5))
+	# print(lp)
 
 	for c in val_counts.keys():
-		a6[c]=-1*math.log2((val_counts[c]+0.1)/len(data)+0.3)
+		numerator=val_counts[c]+0.1
+		denominator=len(data)+0.3
+		a6[c]=-1*math.log2(numerator/denominator)
 		data_=data[data.a6==c]
 		for a in range(1,6):
 			a_col="a"+str(a)
 			grouped_counts=data_[[a_col,"a6"]].groupby(a_col).count().to_dict()["a6"]
-			if DEBUG:
-				print("grouped_counts for c=" + str(c) + " and " + a_col)
-				print(grouped_counts)
 			for val in range(1,5):
-				lp[c][a][val]=-1*math.log2((grouped_counts[val]+0.1)/val_counts[c]+0.4)
+				if val in grouped_counts.keys():
+					v = grouped_counts[val]
+				else:
+					v=0
+				numerator = v+0.1
+				denominator = val_counts[c]+0.4
+				lp[c][a][val]=-1*math.log2(numerator/denominator)
 				if DEBUG:
-					print("count for c=" + str(c) + " and " + a_col + "=" + str(val))
-					print(grouped_counts[val])
-					print(lp[c][a][val])
+					print("count for c=" + str(c) + " and " + a_col + "=" + str(val) + ": " + "grouped_counts[v]=" + str(v))
+			print()
+		print("<------>")
+
+
 	print(lp)
 	return a6, lp
+
+
+def test(data, lp):
+	if DEBUG:
+		print(data)
+	inferences = []
+	for i, row in data.iterrows():
+		get_inference(row[0], row[1], row[2], row[3], row[4], lp)
+
+def get_inference(a1_val, a2_val, a3_val, a4_val, a5_val, lp):
+	if DEBUG:
+		print("get_inference")
+	sums = []
+	for c in range(1,4):
+		s = lp[c][1][a1_val] \
+			+ lp[c][2][a2_val] \
+			+ lp[c][3][a3_val] \
+			+ lp[c][4][a4_val] \
+			+ lp[c][5][a5_val]
+		sums.append(s)
+	if DEBUG:
+		print(sums)
 
 
 
